84226ad4e7c39eb1ce6419069378075a00de3be3
ignaqui <ignaqui@gmail.com>
2016-11-17 09:37:38 -0500

    disable paging

--- lib/oxidized/model/weos.rb
+++ lib/oxidized/model/weos.rb
@@ -1,1 +1,22 @@
+class WEOS < Oxidized::Model
 
+  # Ciena SAOS switch
+  # used for 6.x devices
+ 
+  comment  '! '
+
+  cmd :all do |cfg|
+    cfg.each_line.to_a[1..-2].join
+  end
+
+  cmd 'show running-config' do |cfg|
+    cfg
+  end
+
+  cfg :telnet do
+    username /login:/
+    password /assword:/
+    post_login 'cli more disable'
+    pre_logout 'exit'
+  end
+end
